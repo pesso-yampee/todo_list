a331bdffbbe1198c61cf5633b2fa6356251a8761
yamasaki junpei <opss0622@gmail.com>
2023-05-20 16:00:20 +0900

    feat: 一覧表示を作成

---
+++ src/ui/components/atoms/pageTitle/index.tsx
@@ -0,0 +1,9 @@
+import styles from "ui/components/atoms/pageTitle/style.module.css";
+
+type Props = {
+  text: string;
+};
+
+export function PageTitle({ text }: Props) {
+  return <h1 className={styles.title}>{text}</h1>;
+}
---
+++ src/ui/components/molecules/searchTodoInput/index.tsx
@@ -0,0 +1,5 @@
+import { Input } from "ui/components/atoms/Input";
+
+export function SearchTodoInput() {
+  return <Input placeholder="Search Keyword" />;
+}
