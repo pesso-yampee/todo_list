0e2d5f1e9bb49062694ea93129250ccf0c888333
yamasaki junpei <opss0622@gmail.com>
2023-05-21 13:50:33 +0900

    feat: Todoリストからアイテムを削除する機能を追加

--- src/ui/components/molecules/deleteButton/index.tsx
+++ src/ui/components/molecules/deleteButton/index.tsx
@@ -1,20 +1,37 @@
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "ui/components/atoms/button";
 
-export function DeleteButton() {
-  function clickHandler() {
-    window.alert("clicked!!");
-  }
-  
-  return (
-    <Button
-      type="button"
-      ariaLabel="削除する"
-      clickEvent={clickHandler}
+type Props = {
+  setList: React.Dispatch<
+    React.SetStateAction<
+      {
+        text: string;
+        id: string;
+      }[]
     >
+  >;
+};
+
+export function DeleteButton({ setList }: Props) {
+  function deleteTodo(e: React.MouseEvent<HTMLButtonElement>) {
+    const currentTarget = e.currentTarget;
+    const todoItem = currentTarget.closest("li");
+    const todoItemId = todoItem?.id;
+    const res = window.confirm("本当に削除しますか？");
+
+    if (res) {
+      setList((prevTodos) =>
+        prevTodos.filter((item) => {
+          return item.id !== todoItemId;
+        })
+      );
+    }
+  }
+
+  return (
+    <Button type="button" ariaLabel="delete the todo item" deleteTodo={deleteTodo}>
       <FontAwesomeIcon icon={faTrash} size="1x" />
     </Button>
-  )
+  );
 }
--- src/ui/components/organisms/todoItem/index.tsx
+++ src/ui/components/organisms/todoItem/index.tsx
@@ -1,19 +1,26 @@
-import { v4 as uuidv4 } from "uuid";
 import { DeleteButton } from "ui/components/molecules/deleteButton";
 import styles from "./style.module.css";
 
 type Props = {
-  list: Array<{ text: string }>;
+  list: Array<{ text: string; id: string }>;
+  setList: React.Dispatch<
+    React.SetStateAction<
+      {
+        text: string;
+        id: string;
+      }[]
+    >
+  >;
 };
 
-export function TodoItem({ list }: Props) {
+export function TodoItem({ list, setList }: Props) {
   return (
     <ul className={styles.list}>
       {list.map((item) => (
-        <li key={uuidv4()}>
+        <li key={item.id} id={item.id}>
           <div className={styles.flexContainer}>
             <span className={styles.text}>{item.text}</span>
-            <DeleteButton />
+            <DeleteButton setList={setList} />
           </div>
         </li>
       ))}
