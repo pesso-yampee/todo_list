c575a527dbc48a5c374db57640a2015e4d294fa5
yamasaki junpei <opss0622@gmail.com>
2023-05-21 12:02:34 +0900

    feat: Todoリストの検索機能を追加

--- src/ui/components/atoms/Input/index.tsx
+++ src/ui/components/atoms/Input/index.tsx
@@ -1,19 +1,21 @@
 
-import { KeyboardEvent } from "react";
+import { ChangeEvent, KeyboardEvent } from "react";
 import styles from "ui/components/atoms/Input/style.module.css";
 
 type Props = {
   placeholder: string;
-  keyDownEvent: (e: KeyboardEvent<HTMLInputElement>) => void;
+  keyDownEvent: ((e: KeyboardEvent<HTMLInputElement>) => void) | undefined;
+  changeEvent: ((e: ChangeEvent<HTMLInputElement>) => void) | undefined;
 };
 
-export function Input({ placeholder, keyDownEvent }: Props) {
+export function Input({ placeholder, keyDownEvent, changeEvent }: Props) {
   return (
     <input
       type="text"
       placeholder={placeholder}
       className={styles.input}
       onKeyDown={keyDownEvent}
+      onChange={changeEvent}
     />
   );
 }
