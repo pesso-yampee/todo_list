55535604fcc716c49f8d8f5fa7c6bf7a3a14b0ac
yamasaki junpei <opss0622@gmail.com>
2023-05-21 18:51:55 +0900

    fix: ListPageコンポーネント内のソースをtodoListTemplateに移動

---
+++ src/ui/components/templates/todoListTemplate/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { PageTitle } from "ui/components/atoms/pageTitle";
+import { NewTodoInput } from "ui/components/molecules/newTodoInput";
+import { SearchTodoInput } from "ui/components/molecules/searchTodoInput";
+import { TodoItem } from "ui/components/organisms/todoItem";
+import styles from "./style.module.css";
+
+type TodoProps = {
+  text: string;
+  id: string;
+};
+
+export function TodoListTemplate() {
+  const [list, setList] = useState<TodoProps[]>([]);
+  const [searchText, setSearchText] = useState("");
+
+  const filterList = (() => {
+    const result = list.filter((item) => {
+      return item.text.includes(searchText);
+    });
+    return result;
+  })();
+
+  return (
+    <div className={styles.container}>
+      <PageTitle text="Todo List" />
+      <div className={styles.contents}>
+        <NewTodoInput setList={setList} />
+        <SearchTodoInput setSearchText={setSearchText} />
+        <TodoItem
+          list={searchText === "" ? list : filterList}
+          setList={setList}
+        />
+      </div>
+    </div>
+  );
+}
--- src/ui/pages/listPage/index.tsx
+++ src/ui/pages/listPage/index.tsx
@@ -1,37 +1,5 @@
-import { useState } from "react";
-import { PageTitle } from "ui/components/atoms/pageTitle";
-import { NewTodoInput } from "ui/components/molecules/newTodoInput";
-import { SearchTodoInput } from "ui/components/molecules/searchTodoInput";
-import { TodoItem } from "ui/components/organisms/todoItem";
-import styles from "./style.module.css";
-
-type TodoProps = {
-  text: string;
-  id: string;
-};
+import { TodoListTemplate } from "ui/components/templates/todoListTemplate";
 
 export function ListPage() {
-  const [list, setList] = useState<TodoProps[]>([]);
-  const [searchText, setSearchText] = useState("");
-
-  const filterList = (() => {
-    const result = list.filter((item) => {
-      return item.text.includes(searchText);
-    });
-    return result;
-  })();
-
-  return (
-    <div className={styles.container}>
-      <PageTitle text="Todo List" />
-      <div className={styles.contents}>
-        <NewTodoInput setList={setList} />
-        <SearchTodoInput setSearchText={setSearchText} />
-        <TodoItem
-          list={searchText === "" ? list : filterList}
-          setList={setList}
-        />
-      </div>
-    </div>
-  );
+  return <TodoListTemplate />;
 }
