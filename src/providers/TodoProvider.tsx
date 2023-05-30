c389ccf02b3226ddbf911332314c8d7cc357d95d
yamasaki junpei <opss0622@gmail.com>
2023-05-30 16:24:01 +0900

    update: カスタムフックの内容を更新、参照するカスタムフックを追加

--- src/providers/TodoProvider.tsx
+++ src/providers/TodoProvider.tsx
@@ -6,21 +6,33 @@ type Props = {
 };
 
 type TodoProps = {
-  text: string;
+  title: string | null | undefined;
+  content: string | null | undefined;
   id: string;
 };
 
 type TodoStateProps = {
   list: TodoProps[];
-  searchText: string;
+  searchTodoTitle: string;
+  todo: TodoProps;
+  todoTitle: string;
+  todoContent: string;
 };
 
 type TodoDispatchProps = {
-  useAddToList: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  setTodo: React.Dispatch<React.SetStateAction<TodoProps>>;
+  useInputTodoTitle: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  useInputTodoContent: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  useAddToList: (e: React.MouseEvent<HTMLButtonElement>) => void;
   useSetSearchTodo: (e: React.ChangeEvent<HTMLInputElement>) => void;
   useDeleteTodo: (e: React.MouseEvent<HTMLButtonElement>) => void;
   useFilterList: () => TodoProps[];
   useInitializeList: () => TodoProps[];
+  useFindTodoDetail: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  useSetTodoTitleAndContentIntoField: (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => void;
+  useUpdateTodo: () => void;
 };
 
 export const TodoStateContext = createContext({} as TodoStateProps);
@@ -29,23 +41,40 @@ export const TodoDispatchContext = createContext({} as TodoDispatchProps);
 export function TodoProvider({ children }: Props) {
   const {
     list,
-    searchText,
+    searchTodoTitle,
+    todo,
+    todoTitle,
+    todoContent,
+    setTodo,
+    useInputTodoTitle,
+    useInputTodoContent,
     useAddToList,
     useSetSearchTodo,
     useDeleteTodo,
     useFilterList,
     useInitializeList,
+    useFindTodoDetail,
+    useSetTodoTitleAndContentIntoField,
+    useUpdateTodo,
   } = useTodo();
 
   return (
-    <TodoStateContext.Provider value={{ list, searchText }}>
+    <TodoStateContext.Provider
+      value={{ list, searchTodoTitle, todo, todoTitle, todoContent }}
+    >
       <TodoDispatchContext.Provider
         value={{
+          setTodo,
+          useInputTodoTitle,
+          useInputTodoContent,
           useAddToList,
           useSetSearchTodo,
           useDeleteTodo,
           useFilterList,
           useInitializeList,
+          useFindTodoDetail,
+          useSetTodoTitleAndContentIntoField,
+          useUpdateTodo,
         }}
       >
         {children}
