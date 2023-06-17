84eb0242a7e069ee44883d9f2a8b1a30b724dc30
yamasaki junpei <opss0622@gmail.com>
2023-06-17 22:24:26 +0900

    update: ページパスを定義

---
+++ src/constants/pagePath.tsx
@@ -0,0 +1,24 @@
+/**
+ * リンク先一覧
+ * 遷移先定義の際に使用
+ * @type {{TOP: string, CREATE: string, DETAIL: string, EDIT: string}}
+ */
+export const PAGE_LIST = {
+  TOP: "/",
+  CREATE: "/create",
+  DETAIL: `/detail/:id`,
+  EDIT: `/edit/:id`,
+};
+
+/**
+ * パス一覧
+ * 画面遷移時に使用
+ * @type {{TOP: string, CREATE: string, DETAIL: string, EDIT: string}}
+ */
+
+export const PAGE_PATH = {
+  TOP: "/",
+  CREATE: "/create",
+  DETAIL: `/detail/`,
+  EDIT: `/edit/`,
+};
