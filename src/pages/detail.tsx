4b9dc5bcf84baec45e4f780a60c5c824ef6f80a1
yamasaki junpei <opss0622@gmail.com>
2023-06-17 22:18:19 +0900

    delete: 不要なファイルを削除

--- src/pages/detail.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-import { DetailTodoTemplate } from "components/templates/detailTodoTemplate";
-
-const DetailTodoPage: NextPage = () => {
-  return <DetailTodoTemplate text="TodoDetail" />;
-};
-export default DetailTodoPage;
--- src/pages/edit.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-import { EditTodoTemplate } from "components/templates/editTodoTemplate";
-
-const EditTodoPage: NextPage = () => {
-  return <EditTodoTemplate text="TodoEdit" />;
-};
-export default EditTodoPage;
