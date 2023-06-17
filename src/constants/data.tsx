4af1a51a76ae522a256c72882d61b2348157d4e0
yamasaki junpei <opss0622@gmail.com>
2023-06-17 22:24:08 +0900

    update: デフォルトで表示させるデータを定義

---
+++ src/constants/data.tsx
@@ -0,0 +1,7 @@
+/**
+ * デフォルトで表示させておくデータ
+ */
+export const INITIAL_DATA = [
+  { title: "todo1", content: "todo1", id: "awrsgdhfjghk" },
+  { title: "todo2", content: "todo2", id: "oqilukyjfthdgdf" },
+];
