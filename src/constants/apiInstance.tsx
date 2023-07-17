973284482bf053d0d00132207faae1a6e31560f1
yamasaki junpei <opss0622@gmail.com>
2023-07-17 09:35:41 +0900

    feat: apiのインスタンスを共通化するためにファイルを作成

---
+++ src/constants/apiInstance.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const apiInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACK_END,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
