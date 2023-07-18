087c9d8c6709e4f86b1eea407dcd7d0e5dd99707
yamasaki junpei <opss0622@gmail.com>
2023-07-18 19:04:25 +0900

    update: フォームデータを簡単に取得するためにライブラリを追加、corsのオプションを共通化

--- api/routes/taskRoutes.js
+++ api/routes/taskRoutes.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const taskController = require("../controllers/taskController");
 const cors = require("cors");
+const multer = require("multer");
 
-router.get(
-  "/",
-  cors({
-    origin: "http://localhost:3001", // 許可したいオリジンを指定
-    credentials: true, // レスポンスヘッダーにAccess-Control-Allow-Credentialsを追加。ユーザー認証等を行う場合は、これがないとブラウザがレスポンスを捨ててしまうそう。
-  }),
-  taskController.read
-);
-router.post("/", taskController.create);
+const upload = multer();
+
+const corsOption = {
+  origin: "http://localhost:3001", // 許可したいオリジンを指定
+  credentials: true, // レスポンスヘッダーにAccess-Control-Allow-Credentialsを追加。ユーザー認証等を行う場合は、これがないとブラウザがレスポンスを捨ててしまうそう。
+};
+router.get("/", cors(corsOption), taskController.read);
+router.post("/", cors(corsOption), upload.none(), taskController.create);
 router.put("/:id", taskController.update);
 router.delete("/:id", taskController.delete);
 
