a663f41ff22ba1411bff6aec4758d95dbbb6ed1e
yamasaki junpei <opss0622@gmail.com>
2023-07-18 19:08:04 +0900

    update: 共通化できない部分を削除

--- src/constants/apiInstance.tsx
+++ src/constants/apiInstance.tsx
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export const apiInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACK_END,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
