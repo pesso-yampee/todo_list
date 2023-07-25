3eaee8dcd38c93a297d88fbe08bfd0ddea9712d7
yamasaki junpei <opss0622@gmail.com>
2023-07-25 19:06:17 +0900

    delete: 前方一致処理を削除、フォームを削除

--- src/components/templates/listTemplate/index.tsx
+++ src/components/templates/listTemplate/index.tsx
@@ -3,7 +3,6 @@ import { TodoItem } from "components/organisms/todoItem";
 import { Navigation } from "components/organisms/navigation";
 import styles from "./style.module.css";
 import React, { useState } from "react";
-import { TodoType } from "types/todo";
 import { Input } from "components/atoms/Input";
 import useSWR from "swr";
 import { apiClient } from "constants/apiClient";
@@ -29,24 +28,12 @@ export function ListTemplate({ text }: Props) {
 
   const { data } = useSWR("/task", fetcher);
 
-  /**
-   * 前方一致検索でヒットしたTodoを新しいリストとして返す処理
-   */
-  const searchPrefixMatchTodoList = () => {
-    const result: TodoType[] = data.filter((item: TodoType) => {
-      return item.title?.startsWith(todoTitle);
-    });
-
-    return result;
-  };
-
   if (data) {
     return (
       <div className={styles.container}>
         <Navigation />
         <PageTitle text={text} />
         <div className={styles.contents}>
-          <Input placeholder="Title" onChangeHandler={onChangeHandler} />
           <TodoItem list={data} />
         </div>
       </div>
