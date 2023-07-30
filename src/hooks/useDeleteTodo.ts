54b00938336876881fee475c99e731d01338315a
yamasaki junpei <opss0622@gmail.com>
2023-07-30 12:18:32 +0900

    feat: TODOを削除する非同期処理を作成

---
+++ src/hooks/useDeleteTodo.ts
@@ -0,0 +1,15 @@
+import { apiClient } from "constants/apiClient";
+
+export const useDeleteTodo = () => {
+  const doDelete = (todoItemId: string) => {
+    try {
+      apiClient.delete(`/task/${todoItemId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    doDelete,
+  };
+};
