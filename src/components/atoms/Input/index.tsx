27f08c8b2d0f071efcb9f6b22303b039d5208ca3
yamasaki junpei <opss0622@gmail.com>
2023-07-30 10:19:26 +0900

    update: 型とpropsを修正

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -2,17 +2,21 @@ import styles from "components/atoms/Input/style.module.css";
 import React from "react";
 
 type Props = {
-  placeholder: string;
-  onChangeHandler: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder?: string;
+  name?: string;
+  value?: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-export function Input({ placeholder, onChangeHandler }: Props) {
+export function Input({ placeholder, onChange, name, value }: Props) {
   return (
     <input
       type="text"
+      name={name}
       className={styles.input}
       placeholder={placeholder}
-      onChange={onChangeHandler}
+      onChange={onChange}
+      value={value}
     />
   );
 }
