8e188ca45136d1b4487064e79545ee42b6853a5c
yamasaki junpei <opss0622@gmail.com>
2023-07-30 16:48:44 +0900

    feat: 新規作成

---
+++ src/hooks/usePostCraeteTodo.ts
@@ -0,0 +1,20 @@
+import { apiClient } from "constants/apiClient";
+import { FormInputType } from "types/todo";
+
+export const usePostCraeteTodo = () => {
+  const doPost = async (data: FormInputType) => {
+    try {
+      await apiClient.post("/task", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    doPost,
+  };
+};
---
+++ src/hooks/usePutEditTodo.ts
@@ -0,0 +1,28 @@
+import { AxiosError } from "axios";
+import { apiClient } from "constants/apiClient";
+import { FormInputType } from "types/todo";
+
+type Props = {
+  data: FormInputType;
+  todoItemId: string;
+  onSuccess: () => void;
+  onError: (error: unknown) => void;
+};
+
+export const usePutEditTodo = () => {
+  const doPut = async ({ data, todoItemId, onSuccess, onError }: Props) => {
+    try {
+      await apiClient.put(`/task/${todoItemId}`, {
+        title: data.title,
+        contents: data.contents,
+      });
+      onSuccess();
+    } catch (error) {
+      onError(error);
+    }
+  };
+
+  return {
+    doPut,
+  };
+};
