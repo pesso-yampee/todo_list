503707289bda4e9c1a2ba70cc3abf4ad8e42ef07
yamasaki junpei <opss0622@gmail.com>
2023-08-05 09:13:31 +0900

    style: コードをフォーマット

--- api/controllers/taskController.js
+++ api/controllers/taskController.js
@@ -1,14 +1,14 @@
-const { randomUUID } = require("crypto");
-const db = require("../models/index");
-require("date-utils");
+const { randomUUID } = require('crypto')
+const db = require('../models/index')
+require('date-utils')
 
 module.exports = {
   read: async (req, res, next) => {
     try {
-      const result = await db.Task.findAll();
-      res.send(result);
+      const result = await db.Task.findAll()
+      res.send(result)
     } catch (err) {
-      res.status(500).send(err);
+      res.status(500).send(err)
     }
   },
   create: async (req, res, next) => {
@@ -17,15 +17,15 @@ module.exports = {
         title: req.body.title,
         contents: req.body.contents,
         id: randomUUID(),
-        createdAt: new Date().toFormat("YYYY-MM-DD HH24:MI:SS"),
+        createdAt: new Date().toFormat('YYYY-MM-DD HH24:MI:SS'),
         /**
          * TODO: 何故かDB側では日付がセットされている
          */
         updatedAt: null,
-      });
-      res.send(result);
+      })
+      res.send(result)
     } catch (err) {
-      res.status(500).send(err);
+      res.status(500).send(err)
     }
   },
   update: async (req, res, next) => {
@@ -40,10 +40,10 @@ module.exports = {
             id: req.params.id,
           },
         }
-      );
-      res.send(result);
+      )
+      res.send(result)
     } catch (err) {
-      res.status(500).send(err);
+      res.status(500).send(err)
     }
   },
   delete: async (req, res, next) => {
@@ -52,12 +52,12 @@ module.exports = {
         where: {
           id: req.params.id,
         },
-      });
+      })
       res.send({
         result: result,
-      });
+      })
     } catch (err) {
-      res.status(500).send(err);
+      res.status(500).send(err)
     }
   },
-};
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const taskRoutes = require("./taskRoutes");
-const cors = require("cors");
+const express = require('express')
+const router = express.Router()
+const taskRoutes = require('./taskRoutes')
+const usersRoutes = require('./usersRoutes')
+const cors = require('cors')
 
 const corsOption = {
-  origin: "http://localhost:3001", // 許可したいオリジンを指定
+  origin: 'http://localhost:3001', // 許可したいオリジンを指定
   credentials: true, // レスポンスヘッダーにAccess-Control-Allow-Credentialsを追加。ユーザー認証等を行う場合は、これがないとブラウザがレスポンスを捨ててしまうそう。
-};
-router.use("/task", cors(corsOption), taskRoutes);
+}
+router.use('/task', cors(corsOption), taskRoutes)
+router.use('/users', cors(corsOption), usersRoutes)
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-module.exports = router;
+module.exports = router
