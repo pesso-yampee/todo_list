d8e0398c9b1281db0abc26f4777197c7f4b3abe1
yamasaki junpei <opss0622@gmail.com>
2023-08-19 10:41:37 +0900

    feat: ログイン画面のUI作成

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,29 +1,91 @@
-import { Button } from 'components/atoms/Button'
+import { Box, Button, Stack, TextField } from '@mui/material'
 import { usePostLoginUser } from 'hooks/usePostLoginUser'
 import { NextPage } from 'next'
-import { useForm } from 'react-hook-form'
+import Link from 'next/link'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type TextFieldProps = {
+  email: string | null
+  password: string | null
+}
 
 const LoginPage: NextPage = () => {
   const { doPost } = usePostLoginUser()
-  const { handleSubmit, register } = useForm({
+  const [emailValue, setEmailValue] = useState('')
+  const [passwordValue, setPasswordValue] = useState('')
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<TextFieldProps>({
     defaultValues: {
       email: null,
       password: null,
     },
   })
 
-  const onSubmit = () => {
-    console.log(true)
+  const onSubmit: SubmitHandler<TextFieldProps> = (data) => {
+    console.log(data)
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input placeholder="Email" {...register('email')} type="email" />
-      <input placeholder="Password" {...register('password')} type="password" />
-      <Button type="submit" className="button-primary">
-        <span>ログイン</span>
-      </Button>
-    </form>
+    <Box
+      sx={{
+        display: 'flex',
+        maxWidth: 444,
+        paddingInline: '20px',
+        marginInline: 'auto',
+        minHeight: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Stack spacing={4} flex={1}>
+        <Box component="h1" textAlign={'center'}>
+          Sign in
+        </Box>
+        <Stack spacing={3}>
+          <TextField
+            id="outlined-email-input"
+            required
+            label="Email"
+            type="email"
+            value={emailValue}
+            error={'email' in errors}
+            helperText={errors.email?.message}
+            {...register('email')}
+            onChange={(e) => setEmailValue(e.target.value)}
+          />
+          <TextField
+            id="outlined-password-input"
+            required
+            label="Password"
+            type="password"
+            value={passwordValue}
+            error={'password' in errors}
+            helperText={errors.password?.message}
+            {...register('password')}
+            onChange={(e) => setPasswordValue(e.target.value)}
+          />
+          <Stack direction={'row'} justifyContent={'space-between'}>
+            <Link href="#" passHref>
+              <Button variant="text">パスワードを忘れた場合</Button>
+            </Link>
+            <Link href="#" passHref>
+              <Button variant="text">新規登録</Button>
+            </Link>
+          </Stack>
+          <Button
+            variant="contained"
+            type="submit"
+            onClick={handleSubmit(onSubmit)}
+          >
+            ログイン
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
   )
 }
 
