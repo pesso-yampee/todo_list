e5303c739d7ad4cbc5cdea9cb735e3ad68895593
yamasaki junpei <opss0622@gmail.com>
2023-08-26 15:36:14 +0900

    update: ディレクトリ設計を変更

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
-import 'styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
