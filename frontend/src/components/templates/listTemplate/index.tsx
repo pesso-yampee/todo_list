e697992c5bd04b0590d956f48fdca825f478a423
yamasaki junpei <opss0622@gmail.com>
2023-10-01 20:07:35 +0900

    update: データ取得に関するロジックとUIを修正

--- frontend/src/components/templates/listTemplate/index.tsx
+++ frontend/src/components/templates/listTemplate/index.tsx
@@ -1,41 +1,35 @@
+import { Box, CircularProgress, Typography } from '@mui/material'
 import { PageTitle } from 'components/atoms/PageTitle'
 import { Navigation } from 'components/organisms/navigation'
 import { TodoItem } from 'components/organisms/todoItem'
-import { apiClient } from 'constants/apiClient'
-import useSWR from 'swr'
-import styles from './style.module.css'
+import { useFetchTodo } from 'hooks/useFetchTodo'
 
 type Props = {
   text: string
 }
 
-export function ListTemplate({ text }: Props) {
-  const fetcher = async (url: string) => {
-    try {
-      const res = await apiClient.get(url)
-      return res.data
-    } catch (error) {
-      window.alert(error)
-    }
+export const ListTemplate = ({ text }: Props) => {
+  const { data, error, mutate, isLoading } = useFetchTodo()
+  
+  if (isLoading) {
+    return <CircularProgress />
   }
-
-  const { data } = useSWR('/task', fetcher)
-
-  if (data) {
-    return (
-      <div className={styles.container}>
-        <Navigation />
-        <PageTitle text={text} />
-        <div className={styles.contents}>
-          <TodoItem list={data} />
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <p>データがありません。</p>
-      </div>
-    )
-  }
+  return (
+    <Box>
+      <Navigation />
+      <PageTitle text={text} />
+      <Box>
+      {
+        data && !isLoading && (
+        <TodoItem list={data} />
+        )  
+      }
+        {
+          !data && (
+          <Typography>データが存在しません。</Typography>
+          )  
+        }
+      </Box>
+    </Box>
+  )
 }
