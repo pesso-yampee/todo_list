a2e72c2fb7276a928c5cf8ba5471f44996f236bf
yamasaki junpei <opss0622@gmail.com>
2023-10-22 13:56:13 +0900

    delete: 不要になったため削除

--- frontend/src/pages/create.tsx
+++
@@ -1,7 +0,0 @@
-import { CreateTodoTemplate } from 'components/templates/createTodoTemplate'
-import type { NextPage } from 'next'
-
-const CreateTodoPage: NextPage = () => {
-  return <CreateTodoTemplate text="Add New" />
-}
-export default CreateTodoPage
