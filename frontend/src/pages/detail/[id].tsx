6dc7d844fb346b88e96671fabbcec5d1a7b98c78
yamasaki junpei <opss0622@gmail.com>
2023-11-05 15:12:22 +0900

    delete: 不要になったコンポーネントを削除

--- frontend/src/pages/detail/[id].tsx
+++
@@ -1,7 +0,0 @@
-import { DetailTodoCotents } from 'pages/detail/contents'
-import type { NextPage } from 'next'
-
-const DetailTodoPage: NextPage = () => {
-  return <DetailTodoCotents text="TodoDetail" />
-}
-export default DetailTodoPage
--- frontend/src/pages/edit/[id].tsx
+++
@@ -1,7 +0,0 @@
-import { EditTodoContents } from 'pages/edit/contents'
-import type { NextPage } from 'next'
-
-const EditTodoPage: NextPage = () => {
-  return <EditTodoContents text="TodoEdit" />
-}
-export default EditTodoPage
