87e91441b392fb71e971cfd540d132e3c16c6e4e
yamasaki junpei <opss0622@gmail.com>
2023-11-11 21:07:51 +0900

    update: 型とPOST時に叩くURLを修正

--- frontend/src/hooks/usePostDeleteTodo.ts
+++ frontend/src/hooks/usePostDeleteTodo.ts
@@ -1,14 +1,14 @@
 import { apiClient } from 'constants/apiClient'
 
 type Props = {
-  id: string
+  id: number
   onSuccess: () => void
   onError: () => void
 }
 export const usePostDeleteTodo = () => {
   const doPost = ({ id, onSuccess, onError }: Props) => {
     apiClient
-      .post('api/todos/delete', { id: id })
+      .post(`api/todos/delete/${id}`)
       .then(() => onSuccess())
       .catch(() => onError())
   }
