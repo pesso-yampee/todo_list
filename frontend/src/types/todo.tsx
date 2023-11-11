bccd5cce0ce7f88f97e04e1c1f73c405868b6c39
yamasaki junpei <opss0622@gmail.com>
2023-11-11 20:48:26 +0900

    update: 型修正

--- frontend/src/types/todo.tsx
+++ frontend/src/types/todo.tsx
@@ -1,12 +1,14 @@
 export type TodoType = {
   title: string
   detail: string
-  id: string
+  id: number
 }
 
 export type TodoCreateRequest = {
   title: string | null
   detail: string | null
+  id: number
 }
 
 export type TodoUpdateRequest = TodoCreateRequest
+export type TodoUpdateResponse = TodoCreateRequest
