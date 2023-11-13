340c3d9c212c638d8cee91f16aacc7945cd7616b
yamasaki junpei <opss0622@gmail.com>
2023-11-13 22:29:30 +0900

    delete: 削除

--- frontend/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import type { NextPage } from 'next'
-import { TodoListContents } from 'pages/contents'
-
-const TodoListPage: NextPage = () => <TodoListContents text="TodoList" />
-
-export default TodoListPage
