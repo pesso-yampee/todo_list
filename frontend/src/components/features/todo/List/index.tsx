f2fb38ad4f1fc9776b81245195f332868f2e885e
yamasaki junpei <opss0622@gmail.com>
2023-11-15 19:49:35 +0900

    update: 階層移動

---
+++ frontend/src/components/features/todo/List/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { List } from '@mui/material'
+import { useFetchTodoList } from 'hooks/useFetchTodoList'
+import { useState } from 'react'
+import { TodoType } from 'types/todo'
+import { EditTodoModal } from '../Edit/Modal'
+import { TodoItem } from '../Item'
+
+export const TodoList = () => {
+  const { data, refetch } = useFetchTodoList()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [todoId, setTodoId] = useState<string>('')
+
+  return (
+    <>
+      <List>
+        {data?.map((item: TodoType) => (
+          <TodoItem
+            key={item.id}
+            item={item}
+            refetch={refetch}
+            setTodoId={setTodoId}
+            setIsModalOpen={setIsModalOpen}
+          />
+        ))}
+      </List>
+      {isModalOpen && (
+        <EditTodoModal
+          todoId={todoId}
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
+    </>
+  )
+}
