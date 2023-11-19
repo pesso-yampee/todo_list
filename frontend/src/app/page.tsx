bf13abf5946a64ad0731ba4f0b4e4b4a98a68262
yamasaki junpei <opss0622@gmail.com>
2023-11-19 11:09:00 +0900

    update: Suspense部分をdynamic import

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,24 @@
 import { Box,Typography } from '@mui/material'
-import { Loading } from 'components/common/Loading'
 import { AddTodo } from 'components/features/todo/Add'
 import { TodoList } from 'components/features/todo/List'
 
 import { Metadata } from 'next'
-import { Suspense } from 'react'
-import { ErrorBoundary } from 'react-error-boundary'
+import dynamic from 'next/dynamic'
 
 export const metadata: Metadata = {
   title: 'Todo List',
 }
+
+/**
+ * NOTE:
+ * dynamic import を活用することで非同期にモジュールを読み込むことができる。（返り値はPromise）
+ * オプションに {ssr: false} を指定することで、クライアントサイドでレンダリングすることを可能とする。
+ * ページ読み込み時に必要なJSファイルを削減できるため、ページ表示速度改善にもつながる。
+ */
+const DynamicAppSuspense = dynamic(
+  () => import('../components/common/AppSuspense'),
+  {ssr: false}
+)
 
 export default function Page() {
   return (
@@ -18,11 +27,9 @@ export default function Page() {
         TODOリスト
       </Typography>
       <AddTodo />
-      <ErrorBoundary fallback={<h2>Error!!</h2>}>
-        <Suspense fallback={<Loading />}>
-          <TodoList />
-        </Suspense>
-      </ErrorBoundary>
+      <DynamicAppSuspense>
+        <TodoList />
+      </DynamicAppSuspense>
     </Box>
   )
 }
