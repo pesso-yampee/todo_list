b1b201aa7200d1748decd23935595ab4234b2cbd
yamasaki junpei <opss0622@gmail.com>
2023-12-09 18:20:13 +0900

    update: import追加

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,8 @@
-import './globals.css'
+import { Metadata } from 'next'
 import { ReactNode } from 'react'
+import './globals.css'
+import { AppProvider } from './provider'
+
 export const metadata: Metadata = {
   title: 'Todo List',
 }
