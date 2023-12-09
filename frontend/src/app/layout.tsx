158edb0254ff0c2a7ab3725a3d7a52e2304aed79
yamasaki junpei <opss0622@gmail.com>
2023-12-09 18:19:55 +0900

    update: meta情報追加

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import { ReactNode } from 'react'
+export const metadata: Metadata = {
+  title: 'Todo List',
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
