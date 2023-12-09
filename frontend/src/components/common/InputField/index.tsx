f83642c46cce082ce49ae64e54d0571b695ddd78
yamasaki junpei <opss0622@gmail.com>
2023-12-09 19:25:19 +0900

    update: UI修正

--- frontend/src/components/common/InputField/index.tsx
+++ frontend/src/components/common/InputField/index.tsx
@@ -6,35 +6,27 @@ type Props<T extends FieldValues> = {
   control: UseControllerProps<T>['control']
   name: UseControllerProps<T>['name']
   rules: UseControllerProps<T>['rules']
-  label: string
+  placeholder: string
 }
 
 export const InputField = <T extends FieldValues>({
   control,
   name,
   rules,
-  label,
+  placeholder,
 }: Props<T>) => {
-  const { field, fieldState } = useController({ control, name, rules })
-  const { error } = fieldState
+  const { field, fieldState: {error} } = useController({ control, name, rules })
 
   return (
     <div className="grid gap-2">
-      <div className="group relative rounded border-2 border-gray-200">
-        <input
+      <input
           {...field}
           type={'text'}
-          className="group w-full p-2 duration-300 invalid:border-red-500 invalid:text-red-500 focus:pt-8"
+          placeholder={placeholder}
+          className="border-solid border-gray-300 border-2 rounded py-2 px-1 w-full invalid:border-red-500 invalid:text-red-500"
         />
-        <label
-          htmlFor={name}
-          className="absolute left-0 top-0 h-full p-2 group-focus:text-gray-300"
-        >
-          {label}
-        </label>
-      </div>
       <span className="text-sm text-red-500">
-        {error?.message ?? '値を入力してください'}
+        {error?.message && error.message}
       </span>
     </div>
   )
