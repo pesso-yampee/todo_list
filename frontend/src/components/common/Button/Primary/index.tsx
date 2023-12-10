fe3d3ada2f1067626b80e315542e7ddda7380de9
yamasaki junpei <opss0622@gmail.com>
2023-12-10 09:19:56 +0900

    update: propsにtypeを追加

--- frontend/src/components/common/Button/Primary/index.tsx
+++ frontend/src/components/common/Button/Primary/index.tsx
@@ -1,13 +1,19 @@
 type Props = {
   itemId?: string
   text: string
+  type?: 'button' | 'submit'
   onClickEvent?: (id: string) => void
 }
 
-export const PrimaryButton = ({ itemId, text, onClickEvent }: Props) => {
+export const PrimaryButton = ({
+  itemId,
+  text,
+  type = 'button',
+  onClickEvent,
+}: Props) => {
   return (
     <button
-      type="button"
+      type={type}
       className="box-border w-full rounded-md border-2 border-blue-500 bg-blue-500 px-4 py-2 text-base font-bold text-white transition duration-300 ease-in-out hover:bg-white hover:text-blue-500 focus:bg-white focus:text-blue-500"
       onClick={() => onClickEvent && itemId && onClickEvent(itemId)}
     >
