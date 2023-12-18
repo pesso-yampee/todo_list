57e204383f079f72d60508b92759150f27b4ad20
yamasaki junpei <opss0622@gmail.com>
2023-12-18 20:39:30 +0900

    update: propsの受け取り方を修正

--- frontend/src/components/common/Button/Danger/index.tsx
+++ frontend/src/components/common/Button/Danger/index.tsx
@@ -1,15 +1,17 @@
-type Props = {
-  itemId: string
-  text: string
-  onClickEvent: (id: string) => void
-}
+import { HTMLAttributes } from "react"
 
-export const DangerButton = ({ itemId, text, onClickEvent }: Props) => {
+type ButtonProps = HTMLAttributes<HTMLButtonElement>
+
+type Props = {
+  text: string
+} & ButtonProps
+
+export const DangerButton = ({ text, ...buttonProps }: Props) => {
   return (
     <button
       type="button"
       className="box-border w-full rounded-md border-2 border-red-500 bg-red-500 px-4 py-2 text-base font-bold text-white transition duration-300 ease-in-out hover:bg-white hover:text-red-500 focus:bg-white focus:text-red-500"
-      onClick={() => onClickEvent(itemId)}
+      {...buttonProps}
     >
       {text}
     </button>
