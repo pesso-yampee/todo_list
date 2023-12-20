bcd1d4d52a329d31a943936141fd212583f8a645
yamasaki junpei <opss0622@gmail.com>
2023-12-20 21:06:15 +0900

    update: post処理を修正

--- frontend/src/hooks/usePostLoginUser.ts
+++ frontend/src/hooks/usePostLoginUser.ts
@@ -1,8 +1,17 @@
-import { TextFieldProps } from '@mui/material'
 import { apiClient } from 'constants/apiClient'
+import { FieldValues } from 'react-hook-form'
+
+type Props = {
+  data: FieldValues
+  onError: () => void
+  onSuccess: () => void
+}
 
 export const usePostLoginUser = () => {
-      .post('/api/login', data)
+  const doPost = async ({ data, onError, onSuccess }: Props) => {
+    return await apiClient.post('/api/login', data)
+      .then(() => onSuccess())
+      .catch(() => onError())
   }
 
   return {
