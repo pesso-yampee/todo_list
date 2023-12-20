9f7577358e2fc1890df21d42963259c31be3cbe0
yamasaki junpei <opss0622@gmail.com>
2023-12-20 21:07:17 +0900

    update: api通信をするときのオプションを修正

--- frontend/src/constants/apiClient.ts
+++ frontend/src/constants/apiClient.ts
@@ -2,4 +2,6 @@ import axios from 'axios'
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACK_END,
+  withCredentials: true,
+  xsrfCookieName: process.env.NEXT_PUBLIC_FRONT_END?.toUpperCase() + '-XSRF-TOKEN'
 })
