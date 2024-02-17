d5b00321ea0bf3977ee0b86d1062a7d7f6412c97
yamasaki junpei <opss0622@gmail.com>
2024-02-17 10:23:15 +0900

    update: JsonResponseを使用

--- backend/app/Http/Controllers/Api/MeController.php
+++ backend/app/Http/Controllers/Api/MeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class MeController extends Controller
 {
