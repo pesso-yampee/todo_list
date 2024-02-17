0a19e99a490072c4eac4b15ff478b410fb77beb3
yamasaki junpei <opss0622@gmail.com>
2024-02-17 10:27:54 +0900

    update: プロパティ title, user_id が外部から直接アクセスできないようにする

--- backend/app/Models/Todo.php
+++ backend/app/Models/Todo.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'user_id' => 'int'
+    ];
 }
