4425c70c8b4d8ee9d8afc0482a0ec1d75aaebac2
yamasaki junpei <opss0622@gmail.com>
2024-02-23 12:43:04 +0900

    update: ページパス追加

--- frontend/src/constants/pagePath.ts
+++ frontend/src/constants/pagePath.ts
@@ -1,17 +1,5 @@
-/**
- * リンク先一覧
- * 遷移先定義の際に使用
- */
-export const PAGE_LIST = {
-  top: '/',
-  login: '/login',
-}
-
-/**
- * パス一覧
- * 画面遷移時に使用
- */
 export const PAGE_PATH = {
   top: '/',
   login: '/login',
+  register: 'register'
 }
