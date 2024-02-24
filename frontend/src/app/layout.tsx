9ada99eaa1eb33c65f1d233bdb8d6384bfa7600e
yamasaki junpei <opss0622@gmail.com>
2024-02-24 09:22:40 +0900

    update: トーストが表示されるように修正

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,8 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import { Provider as JotaiProvider } from 'jotai'
 import { Metadata } from 'next'
 import { ReactNode } from 'react'
-import './globals.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export const metadata: Metadata = {
   title: 'Todo List',
@@ -13,7 +14,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang='ja'>
       <body>
         <JotaiProvider>
-          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+          <AppRouterCacheProvider>
+            {children}
+            <ToastContainer />
+          </AppRouterCacheProvider>
         </JotaiProvider>
       </body>
     </html>
