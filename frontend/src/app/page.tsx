2595b8f9339f858cd5f46617489956001b7a9865
yamasaki junpei <opss0622@gmail.com>
2024-03-03 09:41:46 +0900

    update: fetchMeの実行と不要なフラグメントを削除

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,12 +5,14 @@ import { HeaderNavigation } from '@/_components/common/header-navigation'
 import { TodoList } from '@/_components/features/todo/list'
 import { useAtomValue } from 'jotai'
 
+import { useAuth } from '@/hooks/useAuth'
 import { todoEditModalAtom } from '@/store'
 import { SanctumGuard } from '@/_components/common/sanctum-guard'
 import { TodoAddArea } from '@/_components/features/todo/add-area'
 import { TodoEditModal } from '@/_components/features/todo/edit-modal'
-import { Box, Container, Typography } from '@mui/material'
+import { Box,Container,Typography } from '@mui/material'
 import dynamic from 'next/dynamic'
+import { useEffect } from 'react'
 
 /**
  * NOTE:
@@ -24,33 +26,42 @@ const DynamicAppSuspense = dynamic(
 )
 
 export default function Page() {
+  const { fetchMe } = useAuth()
   const todoEditModalState = useAtomValue(todoEditModalAtom)
+
+  // HACK:
+  // ログイン時に fetchMe を叩いてるため、結果的に fetchMe を複数回実行していることになる。
+  // こういう設計を許していると、リクエスト数が無駄に跳ね上がり、サーバー側が逼迫する原因になりかねないので、
+  // もっといいやりかたがないか検討する。
+  useEffect(() => {
+    fetchMe()
+    // NOTE: 初回レンダリング時にのみ fetchMe を実行したいため、依存配列は空のままにする
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <SanctumGuard>
-      <>
-        <Container sx={{ marginTop: '56px', paddingTop: '20px' }}>
-          <HeaderNavigation />
-          <Box>
-            <Typography
-              variant={'h1'}
-              gutterBottom={false}
-              textAlign={'center'}
-              fontSize={'24px'}
-              fontWeight={'bold'}
-            >
-              TODOリスト
-            </Typography>
-            <TodoAddArea />
-            <DynamicAppSuspense>
-              <TodoList />
-            </DynamicAppSuspense>
-          </Box>
-        </Container>
-        {todoEditModalState.isOpen && (
-          <TodoEditModal isOpen={todoEditModalState.isOpen} />
-        )}
-      </>
+      <Container sx={{ marginTop: '56px', paddingTop: '20px' }}>
+        <HeaderNavigation />
+        <Box>
+          <Typography
+            variant={'h1'}
+            gutterBottom={false}
+            textAlign={'center'}
+            fontSize={'24px'}
+            fontWeight={'bold'}
+          >
+            TODOリスト
+          </Typography>
+          <TodoAddArea />
+          <DynamicAppSuspense>
+            <TodoList />
+          </DynamicAppSuspense>
+        </Box>
+      </Container>
+      {todoEditModalState.isOpen && (
+        <TodoEditModal isOpen={todoEditModalState.isOpen} />
+      )}
     </SanctumGuard>
   )
 }
