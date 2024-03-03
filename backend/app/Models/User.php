68a7d30db52a14e228e06c5cd9f4efd57923c96d
yamasaki junpei <opss0622@gmail.com>
2024-03-03 11:25:59 +0900

    update: #35 [BE]: ログインユーザーによって表示するタスクを限定する

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function todos() {
+        return $this->hasMany(Todo::class);
+    }
 }
