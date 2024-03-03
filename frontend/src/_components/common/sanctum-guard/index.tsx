90c5ea8e31babb3fc15db034fcc86cf17147160b
yamasaki junpei <opss0622@gmail.com>
2024-03-03 09:54:49 +0900

    update: isAuthenticatedの定義元と初回レンダリング時に実行する内容を修正

--- frontend/src/_components/common/sanctum-guard/index.tsx
+++ frontend/src/_components/common/sanctum-guard/index.tsx
@@ -1,8 +1,7 @@
 import { PAGE_PATH } from '@/constants/pagePath'
-import { isAuthenticatedAtom } from '@/store'
-import { useAtomValue } from 'jotai'
+import { useAuth } from '@/hooks/useAuth'
 import { useRouter } from 'next/navigation'
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 import { toast } from 'react-toastify'
 
 type Props = {
@@ -11,12 +10,14 @@ type Props = {
 
 export const SanctumGuard = ({ children }: Props) => {
   const router = useRouter()
-  const isAuthenticated = useAtomValue(isAuthenticatedAtom)
+  const { isAuthenticated } = useAuth()
 
-  if (!isAuthenticated) {
+  useEffect(() => {
+    if (isAuthenticated) return
+
     toast.error('認証エラー')
     router.push(PAGE_PATH.login)
-  }
+  }, [isAuthenticated, router])
 
   return <>{children}</>
 }
