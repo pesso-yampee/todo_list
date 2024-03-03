47f265cdea6629781c99e004d4be738d5b549ecd
yamasaki junpei <opss0622@gmail.com>
2024-03-03 09:31:20 +0900

    update: doPostの内部処理を修正

--- frontend/src/hooks/usePostLoginUser.ts
+++ frontend/src/hooks/usePostLoginUser.ts
@@ -1,47 +1,25 @@
 import { apiClient } from '@/constants/apiClient'
-import { isAuthenticatedAtom } from '@/store'
-import { AxiosResponse } from 'axios'
-import { useSetAtom } from 'jotai'
+import { useAuth } from '@/hooks/useAuth'
 import { FieldValues } from 'react-hook-form'
-
-type LoginResponse = {
-  id: number
-  name: string
-  email: string
-  email_verified_at: Date
-  created_at: Date
-  updated_at: Date
-}
 
 type Props = {
   data: FieldValues
   onError: () => void
-  onSuccess: (response: AxiosResponse<LoginResponse>) => void
+  onSuccess: () => void
 }
 
 export const usePostLoginUser = () => {
-  const setIsAuthenticated = useSetAtom(isAuthenticatedAtom)
+  const { fetchMe } = useAuth()
 
-  const fetchMe = async () => {
-    return apiClient
-      .get('api/me')
+  const doPost = async ({ data, onError, onSuccess }: Props) => {
+    await apiClient.get('/sanctum/csrf-cookie')
+    await apiClient
+      .post('/api/login', data)
       .then(() => {
-        setIsAuthenticated(true)
+        onSuccess()
+        fetchMe()
       })
-      .catch(() => {
-        setIsAuthenticated(false)
-      })
-  }
-
-  const doPost = ({ data, onError, onSuccess }: Props) => {
-    apiClient.get('/sanctum/csrf-cookie').then(async () => {
-      await apiClient
-        .post('/api/login', data)
-        .then((response: AxiosResponse<LoginResponse>) => onSuccess(response))
-        .catch(() => onError())
-
-      await fetchMe()
-    })
+      .catch(() => onError())
   }
 
   return {
